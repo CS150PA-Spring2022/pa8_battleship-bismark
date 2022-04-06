9788ed53cad7194545ecf5b9c5e3f2bade36e4f9
Holonium <holonium2.0@gmail.com>
2022-04-06 14:35:29 -0700

    Remove merge meta.

--- battleship.cpp
+++ battleship.cpp
@@ -89,7 +89,6 @@ void computerMove(int thisMove[], int lastMove[], char history[][NUM_COLS], bool
 	// Output move
 }
 
-<<<<<<< HEAD
 void checkSurrounding(int thisMove[], int lastMove[], char history[][NUM_COLS], int lastHit[], char target){
 	char lastHitShip = history[lastHit[0]][lastHit[1]];
 	thisMove = {-1,-1};
@@ -133,12 +132,12 @@ void checkSurrounding(int thisMove[], int lastMove[], char history[][NUM_COLS], 
 			
 		default : break;
 	}*/
-=======
+}
+
 bool checkYN(char input){
 	if (input == 'Y' || input == 'y'){
 		return true;
 	} else {
 		return false;
 	}
->>>>>>> main
 }
