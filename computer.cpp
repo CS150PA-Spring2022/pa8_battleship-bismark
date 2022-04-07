ba0e006b06664a4fc17710cbeb9f1a88bc1c6b92
Holonium <holonium2.0@gmail.com>
2022-04-07 12:49:00 -0700

    Added computer.cpp file and core elements.

---
+++ computer.cpp
@@ -0,0 +1,181 @@
+/**
+ * @file   battleship.cpp
+ * @author Kellen West
+ * @brief  
+ * @date   04-07-2022
+ * 
+ */
+
+#include "battleship.h"
+
+/**
+ * @brief Handles computer player moves
+ *
+ * @param thisMove		  Holds the current move by the computer
+ * @param lastMove		  Stores the last move to allow for smarter play
+ * @param history		  Stores the move statistics of the computer for smarter play
+ */
+ 
+void computerMove(int thisMove[], int lastMove[], int lastHit[], char history[][NUM_COLS], bool liveShips[]){
+	/*bool firstMove = true;
+	// Check if previous move.
+	for (int i = 0; i < NUM_COLS; i++){
+		for (int j = 0; j < NUM_ROWS; j++){
+			if (history[i][j] != '-') firstMove = false;
+		}
+	}
+	do {
+		if (firstMove){
+			srand(time(NULL));
+			// Random move
+			thisMove[0] = rand() % NUM_ROWS; // Random row
+			thisMove[1] = rand() % NUM_COLS; // Random column
+		} else {
+			if (history[lastMove[0]][lastMove[1]] == 'm'){
+				srand(time(NULL));
+				// Random move
+				thisMove[0] = rand() % NUM_ROWS; // Random row
+				thisMove[1] = rand() % NUM_COLS; // Random column
+			} else {
+				char lastResult = history[lastMove[0]][lastMove[1]];
+				int index = 0;
+				for (index = 0; index < NUM_SHIPS; index++){
+					if (lastResult == SHIP_SYMBOLS[index]) break;
+				}
+				if (!liveShips[index]){
+					srand(time(NULL));
+					// Random move
+					thisMove[0] = rand() % NUM_ROWS; // Random row
+					thisMove[1] = rand() % NUM_COLS; // Random column
+				} else {
+					switch (index) {
+						case 0 : // Carrier
+							int hits[5][2];
+							int hitIndex = 0;
+							for (int i = 0; i < NUM_COLS; i++){
+								for (int j = 0; j < NUM_ROWS; j++){
+									if (history[i][j] == SHIP_SYMBOLS[index]){
+										hits
+									}
+								}
+							}
+						case 1 : // Battleship
+							
+						case 2 : // Cruiser
+							
+						case 3 : // Submarine
+							
+						case 4 : // Destroyer
+							
+						default : break;
+					}
+				}
+			}
+		}
+	} while (1);
+	*/
+}
+/*void computerMove(int thisMove[], int lastMove[], char history[][NUM_COLS], bool liveShips[], int lastHit[]){ // NOTE: THIS FUNCTION IS IN ROUGH STAGES. PARAMETERS ARE NOT FIXED!
+	bool firstMove = true;
+	for (int i = 0; i < NUM_COLS; i++){
+		for (int j = 0; j < NUM_ROWS; j++){
+			if (history[i][j] != '-') firstMove = false;
+		}
+	}
+	do {
+		if (firstMove){
+			srand(time(NULL));
+			// Random move
+			thisMove[0] = rand() % NUM_ROWS; // Random row
+			thisMove[1] = rand() % NUM_COLS; // Random column
+		} else {
+			// Begin smart search
+			if (history[lastMove[0]][lastMove[1]] != 'm'){ // Check that last move is a hit.
+				char lastResult = history[lastMove[0]][lastMove[1]];
+				if (lastResult == 'C'){
+					if (liveShips[0]){ // Check if it is still up.
+						if (history[lastMove[0] - 1][lastMove[1]] == 'C'){ // Check if it is in previous row.
+							for (int i = 2; i < SHIP_SIZES[0]; i++){
+								if (history[lastMove[0] - i][lastMove[1]] == '-'){
+									thisMove[0] = lastMove[0] - i;
+									thisMove[1] = lastMove[1];
+								}
+							}
+						} else if (history[lastMove[0] + 1][lastMove[1]] == 'C'){ // Check if it is in the next row.
+							for (int i = 2; i < SHIP_SIZES[0]; i++){
+								if (history[lastMove[0] + i][lastMove[1]] == '-'){
+									thisMove[0] = lastMove[0] + i;
+									thisMove[1] = lastMove[1];
+								}
+							}
+						}
+					}
+				} else if (lastResult == 'B'){
+					if (liveShips[1]){
+					
+					}
+				} else if (lastResult == 'R'){
+					if (liveShips[2]){
+					
+					}
+				} else if (lastResult == 'S'){
+					if (liveShips[3]){
+					
+					}
+				} else if (lastResult == 'D'){
+					if (liveShips[4]){
+					
+					}
+				}
+			}
+			// If no hits on unsunk ships, then pick another random.
+			// If not hit, but not sunk, try other direction.
+		}
+	} while ( Check if valid move 1);
+	// Output move
+}*/
+
+void checkSurrounding(int thisMove[], int lastMove[], char history[][NUM_COLS], int lastHit[], char target){
+	char lastHitShip = history[lastHit[0]][lastHit[1]];
+	thisMove = {-1,-1};
+	// Bring neighbors out of array.
+	char nextRow = history[lastHit[0] + 1][lastHit[1]];
+	char lastRow = history[lastHit[0] - 1][lastHit[1]];
+	char nextCol = history[lastHit[0]][lastHit[1] + 1];
+	char lastCol = history[lastHit[0]][lastHit[1] - 1];
+	
+	bool tried[4];
+	// Check which surroundings are set and store to bool.
+	if (lastRow == '-') tried[0] = false;
+	else if (lastRow == lastHitShip) thisMove = {lastHit[0] + 1, lastHit[1]};
+	if (nextCol == '-') tried[1] = false;
+	else if (nextCol == lastHitShip) thisMove = {lastHit[0], lastHit[1] - 1};
+	if (nextRow == '-') tried[2] = false;
+	else if (nextRow == lastHitShip) thisMove = {lastHit[0] - 1, lastHit[1]};
+	if (lastCol == '-') tried[3] = false;
+	else if (lastCol == lastHitShip) thisMove = {lastHit[0], lastHit[1] + 1};
+	
+	// If move found, try again.
+	if (thisMove[0] != -1 && thisMove[1] != -1) return;
+	
+	//ADD CODE TO CHECK FOR LINE.
+	/*switch (lastHit) {
+		case 'c' :
+			// Check if any neighboring.
+			if (history[lastHit[0] + 1][lastHit[1]] == 'c' || history[lastMove[0] - 1][lastMove[1]] == 'c' || history[lastMove[0]][lastMove[1] + 1] == 'c' || history[lastMove[0]][lastMove[1] -1]){
+				// Check which one it is at.
+			} else {
+				// If none, check which ones already tried, then try next one going CW.
+				
+			}
+		case 'b' :
+			
+		case 'r' :
+			
+		case 's' :
+			
+		case 'd' :
+			
+		default : break;
+	}*/
+}
