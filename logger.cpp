8c4763bd44b4cc50c4ed5d83eed0de875213c7ca
EdwardSJ <lego.edward@gmail.com>
2022-04-20 18:36:18 -0700

    fixed logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -12,72 +12,36 @@
  * @brief A class-based logging system
  */
 
-class Logger {
-	public:
-        
-        /**
-         * @brief Inserts a line to describe how the player's ships were placed.
-         * 
-         * @param auto If the player placed their ships automatically.
-         */
-		void shipsPlaced(bool autoplace){
-			logFile << "Player's ships placed ";
-			if (autoplace){
-				logFile << "automatically.";
-			} else {
-				logFile << "manually.";
-			}
-			logFile << endl;
-			return;
-		}
-
-        /**
-         * @brief Logs a shot.
-         * 
-         * @param row, col: row and column of the shot being logged
-         * @param hit: if the shot being logged hit something
-         * @param mark: the character that the shot resulted in.
-         */
-		void addMove(int row, int col, bool hit, char mark){
-			char rowRef;
-			rowRef = 'A' + row;
-            if (playerTurn){
-				logFile << "Player shoots: " + rowRef + col << ", ";
-			} else {
-				logFile << "Computer shot: " + rowRef + col << ", ";
-			}
-			if (hit){
-				logFile << "Hit! " << mark;
-			} else {
-				logFile << "Miss.";
-            }
-			logFile << endl;
-			playerTurn = !playerTurn;
-			return;
-		};
-
-        // closes file - MUST BE CALLED AT END OF MAIN
-		void finish(){
-			logFile.close();
-			return;
-		};
-
-        /**
-         * @brief Construct a new Logger object.
-         * 
-         * @details When creating the object, the filename will be the current time in seconds
-         */
-		Logger(){
-			string fname;
-			time_t ref = time(NULL);
-			fname = ctime(&ref);
-			logFile.open(fname);
-			logFile << "Boards initialized at " << fname << "." << endl;
-			playerTurn = true;
-		};
-
-	private:
-		fstream logFile;
-		bool playerTurn;
+void Logger::shipsPlaced(bool autoplace){
+	logFile << "Player's ships placed ";
+	if (autoplace){
+		logFile << "automatically.";
+	} else {
+		logFile << "manually.";
+	}
+	logFile << endl;
+	return;
 };
 
+void Logger::addMove(int row, int col, bool hit, char mark){
+	char rowRef;
+	rowRef = 'A' + row;
+	if (playerTurn){
+		logFile << "Player shoots: " + rowRef + col << ", ";
+	} else {
+		logFile << "Computer shot: " + rowRef + col << ", ";
+	}
+	if (hit){
+		logFile << "Hit! " << mark;
+	} else {
+		logFile << "Miss.";
+	}
+	logFile << endl;
+	playerTurn = !playerTurn;
+	return;
+};
+
+void Logger::finish(){
+	logFile.close();
+	return;
+};
